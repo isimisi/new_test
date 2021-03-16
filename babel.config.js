abe5e7b282c2e5ee5f5690a1663941e4ce7a074f
Keanottesen <47158972+Keanottesen@users.noreply.github.com>
2021-03-16 19:38:53 +0100

    init new design

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,34 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-      presets: ["@babel/preset-react"],
-    };
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    'styled-components',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-export-default-from'
+  ],
+  env: {
+    production: {
+      only: ['app'],
+      plugins: [
+        'lodash',
+        'transform-react-remove-prop-types',
+        '@babel/plugin-transform-react-inline-elements',
+        '@babel/plugin-transform-react-constant-elements',
+      ],
+    },
+    test: {
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        'dynamic-import-node',
+      ],
+    },
+  },
 };
