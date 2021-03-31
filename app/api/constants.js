569a03bbd5d922cfa3761dbaa5a38f14933bea57
Keanottesen <47158972+Keanottesen@users.noreply.github.com>
2021-03-31 17:19:55 +0200

    ic

--- app/api/constants.js
+++ app/api/constants.js
@@ -1,6 +1,6 @@
 import { loadFromLocalStorage } from './localStorage/localStorage';
 
-export const baseUrl = 'http://127.0.0.1:3333';
+export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://juristic-api-gateway.vercel.app' : 'http://127.0.0.1:3333';
 
 export const isAuthenticated = () => {
   const tokenMatch = /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.[A-Za-z0-9-_.+/=]*$/;
