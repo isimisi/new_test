047c8ebdd66141b132c885c454e2ed34e40a4265
juristic-christian <86610216+juristic-christian@users.noreply.github.com>
2021-07-08 11:28:31 +0200

    Nyhed

--- app/api/dummy/newsData.js
+++ app/api/dummy/newsData.js
@@ -3,7 +3,7 @@ const newsData = [
   {
     title: 'Velkommen til Juristic (beta)',
     desc: 'Tak for være med! Vi har arbejdet hårdt for at udvikle platformen, så den både er let og intuitiv at bruge. Hvis du har brug for en lille introduktion, har vi udarbejdet en lille guide.',
-    thumb: imgData[45]
+    thumb: 'https://app-juristic-media.s3.eu-north-1.amazonaws.com/racoon/Screenshot+2021-07-08+at+11.05.04.png'
   },
 
 ];
