4ebc38739060899790bf3bb10c3a99f758cbe1fe
juristic-christian <86610216+juristic-christian@users.noreply.github.com>
2021-07-08 11:24:36 +0200

    Nyhed

--- app/api/dummy/newsData.js
+++ app/api/dummy/newsData.js
@@ -2,7 +2,7 @@ import imgData from '../images/photos';
 const newsData = [
   {
     title: 'Velkommen til Juristic (beta)',
-    desc: 'Tak for være med! Vi har arbejdet hårdt for at udvikle platformen, så den både er let og intuitiv at bruge. Hvis du har brug for en lille introduktion, har vi udarbejdet en lille guide over de enkelte funktioner, som du kan finde ved at trykke på nedenstående link.',
+    desc: 'Tak for være med! Vi har arbejdet hårdt for at udvikle platformen, så den både er let og intuitiv at bruge. Hvis du har brug for en lille introduktion, har vi udarbejdet en lille guide.',
     thumb: imgData[45]
   },
 
