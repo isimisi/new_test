ba0913a5f8ff5dcab81b0bb289618fe1d6ecd1b2
Keanottesen <47158972+Keanottesen@users.noreply.github.com>
2021-07-08 16:36:45 +0200

    ic

--- app/api/dummy/guideData.js
+++ app/api/dummy/guideData.js
@@ -2,27 +2,27 @@ const guideSteps = [
   {
     title: 'Velkommen til Juristic',
     label: 'Juristic er en visual platform for rådgivere.',
-    imgPath: '/images/guide/1.jpg',
+    imgPath: 'https://app-juristic-media.s3.eu-north-1.amazonaws.com/guides/1.jpg',
   },
   {
     title: 'CVR-import',
     label: 'Med vores CVR-funktion kan du lynhurtigt få et overblik.',
-    imgPath: '/images/guide/2.gif',
+    imgPath: 'https://app-juristic-media.s3.eu-north-1.amazonaws.com/guides/2.gif',
   },
   {
     title: 'Red flags',
     label: 'Med red flags får du input, mens du drafter.',
-    imgPath: '/images/guide/3.gif',
+    imgPath: 'https://app-juristic-media.s3.eu-north-1.amazonaws.com/guides/3.gif',
   },
   {
     title: 'Få et bedre overblik',
     label: 'Med vores visuelle værktøj får du bedre overblik over problemstillinger.',
-    imgPath: '/images/guide/4.gif',
+    imgPath: 'https://app-juristic-media.s3.eu-north-1.amazonaws.com/guides/4.gif',
   },
   {
     title: 'Få dit eget indhold eller 1-kliks-rapporter',
     label: 'Opgrader i dag for at få flere funktioner med Base, Draw og Pro.',
-    imgPath: '/images/guide/5.jpg',
+    imgPath: 'https://app-juristic-media.s3.eu-north-1.amazonaws.com/guides/5.jpg',
   },
 ];
 
