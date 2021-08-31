30a8b5c5a7e8bedf7debc08d638bcfa481269cb6
Keanottesen <47158972+Keanottesen@users.noreply.github.com>
2021-08-31 13:28:37 +0200

    ic

--- app/api/constants.js
+++ app/api/constants.js
@@ -2,7 +2,7 @@
 import CryptoJS from 'crypto-js';
 import { loadFromLocalStorage } from './localStorage/localStorage';
 
-export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://juristic-web-app.herokuapp.com' : 'http://127.0.0.1:3333';
+export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://juristic-api-gateway.herokuapp.com' : 'http://127.0.0.1:3333';
 
 export const isAuthenticated = () => {
   const tokenMatch = /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.[A-Za-z0-9-_.+/=]*$/;
