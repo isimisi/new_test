63177bc562dcc8807dd5123eeca9843a98f2e8b9
Nemanja <nemo.spaske@Live.com>
2021-11-24 09:59:11 +0100

    baseUrl put back to origin

--- app/api/constants.ts
+++ app/api/constants.ts
@@ -6,13 +6,12 @@ import { loadFromLocalStorage } from '@utils/localStorage';
 import CryptoJS from 'crypto-js';
 import { History } from 'history';
 
-export const baseUrl = 'https://juristic-api-gateway-staging.herokuapp.com';
-// window.location.hostname === 'juristic-web-app-staging.herokuapp.com'
-//   ? 'https://juristic-api-gateway-staging.herokuapp.com'
-//   : process.env.NODE_ENV === 'production'
-//     ? 'https://juristic-api-gateway.herokuapp.com'
-//     : 'http://127.0.0.1:3333';
-
+export const baseUrl = window.location.href === 'https://juristic-web-app-staging.herokuapp.com/'
+  ? 'https://juristic-api-gateway-staging.herokuapp.com'
+  : process.env.NODE_ENV === 'production'
+    ? 'https://juristic-api-gateway.herokuapp.com'
+    : 'http://127.0.0.1:3333';
+    
 export const isAuthenticated = () => {
   const tokenMatch = /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.[A-Za-z0-9-_.+/=]*$/;
   const { token } = loadFromLocalStorage() || {};
