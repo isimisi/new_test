063768cd2836e788aa574cfb7429048bce4a68d9
Nemanja <nemo.spaske@Live.com>
2021-11-29 09:34:26 +0100

    convert to tsx

---
+++ app/containers/Pages/Error/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import brand from '@api/dummy/brand';
+import { Route } from 'react-router-dom';
+import ErrorWrap from '@components/Error/ErrorWrap';
+import { useTranslation } from 'react-i18next';
+
+const title = brand.name + ' - Aplication Error';
+const description = brand.desc;
+
+const Error = () => {
+  const { t } = useTranslation();
+  return (
+    <Route
+      render={({ staticContext }) => {
+        if (staticContext) {
+        // @ts-ignore
+        staticContext.status = 404; // eslint-disable-line 
+        }
+        return (
+          <div>
+            <Helmet>
+              <title>{title}</title>
+              <meta name="description" content={description} />
+              <meta property="og:title" content={title} />
+              <meta property="og:description" content={description} />
+              <meta property="twitter:title" content={title} />
+              <meta property="twitter:description" content={description} />
+            </Helmet>
+            <ErrorWrap
+              desc={t('error.server_goes_wrong')}
+            />
+          </div>
+        );
+      }}
+    />
+  );
+};
+
+export default Error;
