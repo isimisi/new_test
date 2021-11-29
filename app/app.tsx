ddbdd5eb591cbd4cacf774f6cf9650a9175e5347
Keanottesen <47158972+Keanottesen@users.noreply.github.com>
2021-11-29 19:25:01 +0100

    Update app.tsx

--- app/app.tsx
+++ app/app.tsx
@@ -28,8 +28,6 @@ import history from './utils/history';
 import store from './redux/configureStore';
 import './i18n';
 
-import 'video-react/dist/video-react.css';
-
 //  logrocket
 LogRocket.init('pm66tw/juristic-web-app');
 
